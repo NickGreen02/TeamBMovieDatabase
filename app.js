7a7c2b22b559dcc0a999b765ee18b5d40c8ac6da
NickGreen02 <nickgreennbg@gmail.com>
2022-04-04 17:29:56 +0100

    Example query on genre, year and sorted results

--- app.js
+++ app.js
@@ -27,11 +27,13 @@ app.get('/movies', (req, res) => {
     const db = client.db(dbName);
     const collection = db.collection('WebDataCollection');
     // Find some documents
-    collection.find({}).toArray(function(err, movie_list) {
+    collection.find({"genres" : {$regex : "Comedy"}, "release_date" : {$gte : new Date("1995-01-01").toISOString(), $lt : new Date("2000-01-01").toISOString()}}).sort({"vote_average" : -1}).toArray(function(err, movie_list) {
         assert.equal(err, null);
         res.render('movies', {'movies': movie_list})
     });
-
+    
+    //to query based on genre and release date and sort by imdb rating see example below >>>
+    //collection.find({"genres" : {$regex : "Comedy"}, "release_date" : {$gte : new Date("1995-01-01").toISOString(), $lt : new Date("2000-01-01").toISOString()}}).sort({"title" : 1}).toArray(function(err, movie_list)
 
 })
 app.get('/', (req, res) => res.send('Hello World!'))
