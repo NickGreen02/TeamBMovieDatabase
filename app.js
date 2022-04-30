76a29aea5c7056f6115ea512a808c610d902add2
NickGreen02 <nickgreennbg@gmail.com>
2022-04-30 19:39:50 +0100

    Added some filters. Some filters not 100% yet.

--- app.js
+++ app.js
@@ -53,12 +53,19 @@ app.post('/', urlencodedParser, function(req, res){
     console.log(req.body);
     const db = client.db(dbName);
     const collection = db.collection('WebDataCollection');
-    //Find some documents
-    collection.find({$or: [{"title" : {$regex : req.body.movName}}, {"genres" : {$regex : req.body.movName}}]}).limit(4).toArray(function(err, movie_list) {
-        assert.equal(err, null);
-        res.render('movies', {'movies': movie_list})
-    }); 
-
+    //selection statements to check if certain critical fields are empty or not, with respective queries
+    if (req.body.budgetLower == "" || req.body.budgetUpper == "" || req.body.revenueLower == "" || req.body.revenueUpper == ""){    
+        collection.find({$and: [{"title" : {$regex : req.body.movName}}, {"genres" : {$regex : req.body.genreName}}, {"release_date" : {$regex : req.body.yearInput}}]}).limit(4).toArray(function(err, movie_list) {
+            assert.equal(err, null);
+            res.render('movies', {'movies': movie_list})
+        });
+    } 
+    else{
+        collection.find({$and: [{"title" : {$regex : req.body.movName}}, {"genres" : {$regex : req.body.genreName}}, {"release_date" : {$regex : req.body.yearInput}}, {"budget" : {$gte : req.body.budgetLower}}, {"budget" : {$lte : req.body.budgetUpper}}]}).limit(4).toArray(function(err, movie_list) {
+            assert.equal(err, null);
+            res.render('movies', {'movies': movie_list})
+        });
+    }
 });
 
 app.get('/', (req, res) => { 
