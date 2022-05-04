db69683f7d398edce2b5db58674409ba19a1ee1a
NickGreen02 <nickgreennbg@gmail.com>
2022-05-04 15:18:03 +0100

    Filtering now working. Years displayed.

--- app.js
+++ app.js
@@ -56,14 +56,26 @@ app.post('/', urlencodedParser, function(req, res){
     const db = client.db(dbName);
     const collection = db.collection('WebDataCollection');
     //selection statements to check if certain critical fields are empty or not, with respective queries
-    if (req.body.budgetLower == "" || req.body.budgetUpper == "" || req.body.revenueLower == "" || req.body.revenueUpper == ""){    
-        collection.find({$and: [{"title" : {$regex : req.body.movName}}, {"genres" : {$regex : req.body.genreName}}, {"release_date" : {$regex : req.body.yearInput}}]}).limit(4).toArray(function(err, movie_list) {
+    if (req.body.budgetLower == "" && req.body.budgetUpper == "" && req.body.revenueLower == "" && req.body.revenueUpper == ""){    
+        collection.find({$and: [{"title" : {$regex : req.body.movName}}, {"genres" : {$regex : req.body.genreName}}, {"release_date" : {$regex : req.body.yearInput}}]}).limit(10).toArray(function(err, movie_list) {
             assert.equal(err, null);
             res.render('movies', {'movies': movie_list})
         });
     } 
+    else if((req.body.budgetLower == "" && req.body.budgetUpper == "") && (!req.body.revenueLower == "" && !req.body.revenueUpper == "")){
+        collection.find({$and: [{"title" : {$regex : req.body.movName}}, {"genres" : {$regex : req.body.genreName}}, {"release_date" : {$regex : req.body.yearInput}}, {"revenue" : {$gte : req.body.revenueLower}}, {"revenue" : {$lte : req.body.revenueUpper}}]}).limit(10).toArray(function(err, movie_list) {
+            assert.equal(err, null);
+            res.render('movies', {'movies': movie_list})
+        });
+    }
+    else if((!req.body.budgetLower == "" && !req.body.budgetUpper == "") && (req.body.revenueLower == "" && req.body.revenueUpper == "")){
+        collection.find({$and: [{"title" : {$regex : req.body.movName}}, {"genres" : {$regex : req.body.genreName}}, {"release_date" : {$regex : req.body.yearInput}}, {"budget" : {$gte : req.body.budgetLower}}, {"budget" : {$lte : req.body.budgetUpper}}]}).limit(10).toArray(function(err, movie_list) {
+            assert.equal(err, null);
+            res.render('movies', {'movies': movie_list})
+        });
+    }
     else{
-        collection.find({$and: [{"title" : {$regex : req.body.movName}}, {"genres" : {$regex : req.body.genreName}}, {"release_date" : {$regex : req.body.yearInput}}, {"budget" : {$gte : req.body.budgetLower}}, {"budget" : {$lte : req.body.budgetUpper}}]}).limit(4).toArray(function(err, movie_list) {
+        collection.find({$and: [{"title" : {$regex : req.body.movName}}, {"genres" : {$regex : req.body.genreName}}, {"release_date" : {$regex : req.body.yearInput}}, {"budget" : {$gte : req.body.budgetLower}}, {"budget" : {$lte : req.body.budgetUpper}}, {"revenue" : {$gte : req.body.revenueLower}}, {"revenue" : {$lte : req.body.revenueUpper}}]}).limit(10).toArray(function(err, movie_list) {
             assert.equal(err, null);
             res.render('movies', {'movies': movie_list})
         });
