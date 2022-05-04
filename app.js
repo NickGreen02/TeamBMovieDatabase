2955d20eaf8e0bca8889ca5c69cedeff0bc2e076
NickGreen02 <nickgreennbg@gmail.com>
2022-05-04 15:24:36 +0100

    Filtering now works. Years displayed.

--- app.js
+++ app.js
@@ -33,11 +33,7 @@ app.post('/', urlencodedParser, function(req, res){
     const db = client.db(dbName);
     const collection = db.collection('WebDataCollection');
     //selection statements to check if certain critical fields are empty or not, with respective queries
-<<<<<<< HEAD
     if (req.body.budgetLower == "" && req.body.budgetUpper == "" && req.body.revenueLower == "" && req.body.revenueUpper == ""){    
-=======
-    if (req.body.budgetLower == "" || req.body.budgetUpper == "" || req.body.revenueLower == "" || req.body.revenueUpper == ""){    
->>>>>>> 79691d9e9f6ec905dad6e3f0bddcf9cc8d66672c
         collection.find({$and: [{"title" : {$regex : req.body.movName}}, {"genres" : {$regex : req.body.genreName}}, {"release_date" : {$regex : req.body.yearInput}}]}).limit(10).toArray(function(err, movie_list) {
             assert.equal(err, null);
             res.render('movies', {'movies': movie_list})
